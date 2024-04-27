773bcbe56de2b5fe4178f53869c51f466690ad7c
mohamed elamrani <mohammadamrani475@gmail.com>
2024-04-27 09:24:30 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-export default function App() {
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
